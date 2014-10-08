4e0a0d45f4e6cf5beb9f250d037d04dd97a440f5
rajat <rajat.kachhara@commonfloor.com>
2014-10-08 17:47:31 +0530

    log4jclass

---
+++ Log4J.java
@@ -0,0 +1,37 @@
+package com.cf.log4j;
+//import org.apache.log4j.Logger;
+//import org.apache.log4j.PropertyConfigurator;
+//import java.util.Properties;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+ 
+
+public class Log4J
+{
+  /*// our log4j category reference
+  public static Logger log = Logger.getLogger(Log4J.class.getName());
+  static final String LOG_PROPERTIES_FILE = "Property/Log4j.properties";
+ 
+   public Log4J()
+  {
+    initializeLogger();
+    log.info( "Log4JExample - leaving the constructor ..." );
+  }
+ 
+  private void initializeLogger()
+  {
+    Properties logProperties = new Properties();
+ 
+    try
+    {
+      // load our log4j properties / configuration file
+      logProperties.load(new FileInputStream(LOG_PROPERTIES_FILE));
+      PropertyConfigurator.configure(logProperties);
+      log.info("Logging initialized."); 
+    }
+    catch(IOException e)
+    {
+      throw new RuntimeException("Unable to load logging property " + LOG_PROPERTIES_FILE);
+    }
+  }*/
+ }
